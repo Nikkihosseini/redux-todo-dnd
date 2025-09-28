9f2f9572f9768eb4a9047603c4f43eef192f27c3
Nikkihosseini <niekal00@gmail.com>
2025-09-28 19:33:16 +0330

    style(todo): improve todo text display
    - add padding, font size, and responsive text styling
    - handle long text with  line-clamp
    - ensure text wraps properly on different screen sizes
    - enhance readability and overall UI for todo items

--- src/Component/ToDo.jsx
+++ src/Component/ToDo.jsx
@@ -4,16 +4,17 @@
 export default function ToDo(){
     return(
         <>
-            <div className="flex items-center justify-between w-full bg-violet-500/10 min-h-20 max-h-40 px-5 md:px-10 py-3 rounded-3xl overflow-hidden text-sky-900 dark:text-slate-400">
-                <div className="flex items-center gap-x-2 cursor-pointer">
-                    <span className="material-symbols-outlined">
+            <div className="flex items-center justify-between w-full bg-violet-500/10 h-20 px-5 md:px-10 p-4 rounded-3xl text-sky-900 dark:text-slate-400">
+                <div className="flex items-center gap-x-3 h-16">
+                    <span className="material-symbols-outlined cursor-pointer">
                         check_box_outline_blank
                     </span>
-                <p className="text-lg font-semibold py-2 overflow-hidden">
-                     learning java & python ...!
+                <p className="text-base md:text-lg font-semibold line-clamp-2">
+                    git commit -m "feat(ui): add rotation animation to theme toggle button rotate toggle icon 360° when switching between dark and light mode use Tailwind transition and rotate-[360deg] for smooth animation reset rotation after animation completes to allow repeated toggles improves visual feedback and UX for theme switching"
+
                 </p>
                 </div>
-                <span className="material-symbols-outlined">delete</span>
+                <span className="material-symbols-outlined pl-3 cursor-pointer">delete</span>
             </div>
         </>
     )
