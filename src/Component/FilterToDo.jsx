99862c2a0219e04d0d5863efdf8c5664c4e04528
Nikkihosseini <niekal00@gmail.com>
2025-09-30 18:13:42 +0330

    feat(ui): make ToDoApp fully responsive for mobile and desktop using Tailwind CSS

--- src/Component/FilterToDo.jsx
+++ src/Component/FilterToDo.jsx
@@ -1,5 +1,5 @@
 import { useState , useEffect , useRef} from "react";
-import { useDispatch , useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import {setFilter} from './features/filterSlice'
 
 
@@ -40,16 +40,16 @@ export default function FilterToDo(){
     return(
         <>
 
-                <div ref={filterRef} onClick={toggleFilterToDo} className="absolute top-5 left-5 flex items-center justify-center bg-gradient-to-t from-violet-500 to-fuchsia-500 cursor-pointer rounded-md h-10 w-10 transition-all hover:scale-110 z-20">
-                    <span className={`material-symbols-outlined text-white transition-transform duration-500 ${filterBtn ? "rotate-[360deg]" : "rotate-0"}`}>
+                <div ref={filterRef} onClick={toggleFilterToDo} className="absolute top-3 left-3 md:top-5 md:left-5 flex items-center justify-center bg-gradient-to-t from-violet-500 to-fuchsia-500 cursor-pointer rounded-md w-8 h-8 md:h-10 md:w-10 transition-all hover:scale-110 z-20">
+                    <span className={`material-symbols-outlined text-white transition-transform duration-500 text-lg md:text-2xl ${filterBtn ? "rotate-[360deg]" : "rotate-0"}`}>
                         search
                     </span>
                 </div>
 
-                <div ref={filterBtnRef} className={`absolute top-5 left-16 flex items-center justify-center font-semibold text-sky-900 dark:text-slate-400 bg-gradient-to-t from-violet-500/20 to-fuchsia-500/20 h-10 rounded-md gap-x-5 z-10 overflow-hidden transition-transform origin-left ease-in-out duration-300 ${filterBtn ? "scale-x-100 p-2" : "scale-x-0 p-0"}`}>
+                <div ref={filterBtnRef} className={`absolute top-3 md:top-5 left-12 md:left-16 flex items-center justify-center font-semibold text-sky-900 dark:text-slate-400 bg-gradient-to-t from-violet-500/20 to-fuchsia-500/20 h-8 md:h-10 rounded-md gap-x-3 md:gap-x-5 z-10 overflow-hidden transition-transform origin-left ease-in-out duration-300 ${filterBtn ? "scale-x-100 p-2" : "scale-x-0 p-0"}`}>
 
                     {filters.map((filter) => (
-                        <span key={filter} onClick={()=> dispatch(setFilter(filter))} className={`${filterBtn ? "opacity-100 delay-150" : "opacity-0 delay-0"} cursor-pointer dark:hover:text-fuchsia-500 hover:text-fuchsia-700 transition-opacity z-0`}>
+                        <span key={filter} onClick={()=> dispatch(setFilter(filter))} className={`${filterBtn ? "opacity-100 delay-150" : "opacity-0 delay-0"} text-sm md:text-base cursor-pointer dark:hover:text-fuchsia-500 hover:text-fuchsia-700 transition-opacity z-0`}>
                             {filter}
                         </span>
                     ))}
